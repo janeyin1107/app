046481a0ccb7d077527f1e0863ffc69ff5d65ee0
janeyin1107 <jane.jingyuan.yin@gmail.com>
2023-09-29 14:41:58 +1000

    bootcamp

--- bootcamp-app/rds.conf.php
+++ bootcamp-app/rds.conf.php
@@ -1,1 +1,1 @@
-<?php $RDS_URL=""; $RDS_DB=""; $RDS_user=""; $RDS_pwd=""; ?>
+<?php $RDS_URL="demooo-databaseprimaryinstance-9inr8rckkwg9.ch9bvmmlibrp.us-east-1.rds.amazonaws.com"; $RDS_DB="wordpress"; $RDS_user="admin"; $RDS_pwd="password"; ?>
