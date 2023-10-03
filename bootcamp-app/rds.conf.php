88e1d696c7a5f21a95cf64683a125bdfbefdf5d5
janeyin1107 <jane.jingyuan.yin@gmail.com>
2023-10-03 14:33:47 +1100

    bootcamp

--- bootcamp-app/rds.conf.php
+++ bootcamp-app/rds.conf.php
@@ -1,1 +1,1 @@
-<?php $RDS_URL="newendpoint.cluster-custom-ch9bvmmlibrp.us-east-1.rds.amazonaws.com"; $RDS_DB="db"; $RDS_user="admin"; $RDS_pwd="password"; ?>
+<?php $RDS_URL="demo-databasecluster-zsg4xxa0dgyu.cluster-ch9bvmmlibrp.us-east-1.rds.amazonaws.com"; $RDS_DB="db"; $RDS_user="admin"; $RDS_pwd="password"; ?>
